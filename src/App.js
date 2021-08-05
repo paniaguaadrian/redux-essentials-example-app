078e81822e0ffb60d26cd0d5e6cf2c49c5eaf18b
Adrián Paniagua <paniaguasanchezadrian@gmail.com>
2021-08-05 16:52:56 +0200

    Working on new feature: Users List & User Page

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ import { PostsList } from './features/posts/PostsList'
 import { AddPostForm } from './features/posts/AddPostForm'
 import { SinglePostPage } from './features/posts/SinglePostPage'
 import { EditPostForm } from './features/posts/EditPostForm'
+import { UsersList } from './features/users/UsersList'
+import { UserPage } from './features/users/UserPage'
 
 function App() {
   return (
@@ -32,6 +34,9 @@ function App() {
           />
           <Route exact path="/posts/:postId" component={SinglePostPage} />
           <Route exact path="/editpost/:postId" component={EditPostForm} />
+          <Route exact path="/users" component={UsersList} />
+          <Route exact path="/users/:userId" component={UserPage} />
+
           <Redirect to="/" />
         </Switch>
       </div>
