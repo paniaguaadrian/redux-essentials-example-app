345fdead79cbfdfe7d897a097026b0372e730fc3
Adrián Paniagua <paniaguasanchezadrian@gmail.com>
2021-08-06 12:16:57 +0200

    Added new feature: Notifications Badge & List. Error: UserPage.js - user = undefined

--- src/app/store.js
+++ src/app/store.js
@@ -4,10 +4,12 @@ import { configureStore } from '@reduxjs/toolkit'
 // ! This also includes the state of the posts 👀
 import postsReducer from '../features/posts/postsSlice'
 import usersReducer from '../features/users/usersSlice'
+import notificationsReducer from '../features/notifications/notificationsSlice'
 
 export default configureStore({
   reducer: {
     posts: postsReducer,
     users: usersReducer,
+    notifications: notificationsReducer,
   },
 })
